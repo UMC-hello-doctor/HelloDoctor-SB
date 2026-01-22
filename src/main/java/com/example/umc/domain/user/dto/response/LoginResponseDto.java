36fb5102e45ff8bb48c0450afc1851465af7541d
Seeun126 <9035anny@naver.com>
2026-01-22 14:58:24 +0900

    refactor: isNewUser 필드 추가

--- src/main/java/com/example/umc/domain/user/dto/response/LoginResponseDto.java
+++ src/main/java/com/example/umc/domain/user/dto/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
-package com.example.umc.domain.user.dto;
+package com.example.umc.domain.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,11 +11,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonPropertyOrder({"id", "email", "name", "accessToken", "refreshToken"})
+@JsonPropertyOrder({"id", "email", "name", "accessToken", "refreshToken", "isNewUser"})
 public class LoginResponseDto {
     private Long id;
     private String email;
     private String name;
     private String accessToken;
     private String refreshToken;
+
+    @JsonProperty("isNewUser")
+    private boolean newUser; //true -> 프로필 입력 화면
 }
