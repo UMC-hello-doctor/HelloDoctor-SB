ce02817d46a2893ab1901ead5af6744ec6d6dcfc
Seeun126 <9035anny@naver.com>
2026-01-27 21:10:06 +0900

    refactor: 추후 예외 코드를 enum으로 관리하기 위해 리팩토링

--- src/main/java/com/example/umc/global/error/GeneralException.java
+++ src/main/java/com/example/umc/global/error/GeneralException.java
@@ -1,11 +1,22 @@
 package com.example.umc.global.error;
 
-import lombok.AllArgsConstructor;
+import ch.qos.logback.core.status.ErrorStatus;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class GeneralException extends RuntimeException {
     private final String code;
     private final String message;
+
+    public GeneralException(ErrorCode status) {
+        super(status.getMessage());
+        this.code = status.getCode();
+        this.message = status.getMessage();
+    }
+
+    public GeneralException(String code, String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
 }
